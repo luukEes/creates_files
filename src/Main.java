641aed04c6da8424ea9876dd84d0bdd8c90f76d7
Lucas Sowul <sowul.lukas@gmail.com>
2023-01-28 13:24:23 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void createFile(String directoryName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        String directory = directoryName;
+        try {
+            File someFile = new File(directory);
+            if (someFile.createNewFile()) {
+                System.out.println("File created in : " + someFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+        public static void main (String[]args) throws FileNotFoundException {
+        createFile("C:\\Users\\sowul\\creates_files\\kunolis.txt");
+    }
+}
+
